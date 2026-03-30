a0cfe921157a0c76c785db69af114b5ca1d51869
Thijs van Beukering <thijs@Mac-mini-van-Thijs.local>
2026-03-30 15:03:06 +0200

    Update Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,7 +6,7 @@ let _stripe: Stripe | null = null
 export function getStripe(): Stripe {
   if (!_stripe) {
     if (!process.env.STRIPE_SECRET_KEY) throw new Error('STRIPE_SECRET_KEY is not set')
-    _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2025-01-27.acacia' })
+    _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2026-03-25.dahlia' })
   }
   return _stripe
 }
